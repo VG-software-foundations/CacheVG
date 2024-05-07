70a614059383c896a5c26627342f382c0dbc72d3
VictorGalkevich <2vektor54@gmail.com>
2024-05-07 21:01:34 +0300

    added processor mapping

--- src/main/java/cachevg/processor/CommandMapper.java
+++ src/main/java/cachevg/processor/CommandMapper.java
@@ -3,11 +3,17 @@ package cachevg.processor;
 import java.util.HashMap;
 import java.util.Map;
 
+import static cachevg.command.CommandNames.*;
+
 public class CommandMapper {
     private static Map<String, Processor> processors = new HashMap<>();
 
     static {
-
+        processors.put(PUT, new PutCommandProcessor());
+        processors.put(REMOVE, new RemoveCommandProcessor());
+        processors.put(GET, new GetCommandProcessor());
+        processors.put(KEYS, new KeysCommandProcessor());
+        processors.put(PING, new PingCommandProcessor());
     }
 
     public static Processor mapCommandToProcessor(String name) {
